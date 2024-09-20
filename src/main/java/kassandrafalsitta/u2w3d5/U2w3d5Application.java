8364429ce90f8c1a484350fd6dbbd09ad571586d
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-09-20 10:05:40 +0200

    create project

---
+++ src/main/java/kassandrafalsitta/u2w3d5/U2w3d5Application.java
@@ -0,0 +1,13 @@
+package kassandrafalsitta.u2w3d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2w3d5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2w3d5Application.class, args);
+	}
+
+}
---
+++ src/test/java/kassandrafalsitta/u2w3d5/U2w3d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package kassandrafalsitta.u2w3d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2w3d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
